406e90b8b12ff99d9f9b0905c04e2184460b2f2b
Kabir Nahata <kbrnahata@gmail.com>
2025-05-25 17:06:53 +0545

    Create variables_and_types.py

---
+++ 0_intro_to_python/variables_and_types.py
@@ -0,0 +1,17 @@
+# Variables store data in Python
+
+# Integer
+age = 16
+print("Age:", age)
+
+# Float (decimal number)
+height = 5.7
+print("Height:", height)
+
+# String (text)
+name = "Kabir"
+print("Name:", name)
+
+# Boolean (True/False)
+is_student = True
+print("Is a student?", is_student)
