2a1c8e80156fcbc1ea86acef292fa3019de1855e
Kabir Nahata <kbrnahata@gmail.com>
2025-05-31 14:59:04 +0545

    Create built_in_functions.py

---
+++ 2_functions/built_in_functions.py
@@ -0,0 +1,5 @@
+# Examples of built-in Python functions
+
+print("Length of 'Hello':", len("Hello"))   # len() returns length
+print("Rounded value of 3.14159:", round(3.14159, 2))  # round() to 2 decimals
+print("Minimum of [4, 2, 9]:", min([4, 2, 9]))  # min() returns the smallest value
