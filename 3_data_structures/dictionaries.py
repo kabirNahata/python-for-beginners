65fac2d6139d5ddba9737c67b117ebd0e2b695a1
Kabir Nahata <kbrnahata@gmail.com>
2025-05-31 15:00:43 +0545

    Create dictionaries.py

---
+++ 3_data_structures/dictionaries.py
@@ -0,0 +1,11 @@
+# Dictionary: key-value pairs, unordered (as of Python 3.6+, insertion-ordered)
+
+student = {
+    "name": "Kabir",
+    "age": 16,
+    "is_student": True
+}
+
+print("Name:", student["name"])
+student["age"] = 17  # Update value
+print("Updated age:", student["age"])
