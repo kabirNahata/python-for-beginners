149a7eccc02a0b904ca72a30e546bed700efc709
Kabir Nahata <kbrnahata@gmail.com>
2025-05-31 14:59:57 +0545

    Create lists.py

---
+++ 3_data_structures/lists.py
@@ -0,0 +1,9 @@
+# List: ordered, changeable, allows duplicates
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+fruits.append("orange")
+print("After appending:", fruits)
+
+print("First fruit:", fruits[0])
